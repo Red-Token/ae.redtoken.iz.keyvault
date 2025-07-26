a3ad0ee63ddc7aafd3196e1cc4236e55a9fed755
Rene Malmgren <rene.malmgren@redtoken.ae>
2025-07-26 16:30:36 +0400

    an marche!

--- ae.redtoken.iz.keyvault.core/src/test/java/ae/redtoken/iz/keyvault/bitcoin/keymaster/services/protocol/bitcoin/BitcoinConfigurationStackedService.java
+++ ae.redtoken.iz.keyvault.core/src/test/java/ae/redtoken/iz/keyvault/bitcoin/keymaster/services/protocol/bitcoin/BitcoinConfigurationStackedService.java
@@ -2,30 +2,103 @@ package ae.redtoken.iz.keyvault.bitcoin.keymaster.services.protocol.bitcoin;
 
 import ae.redtoken.iz.keyvault.bitcoin.TestWallet;
 import ae.redtoken.iz.keyvault.bitcoin.keymaster.services.protocol.AbstractConfigurationStackedService;
+import ae.redtoken.iz.keyvault.bitcoin.keyvault.KeyVaultProxy;
 import ae.redtoken.util.WalletHelper;
+import org.bitcoinj.base.ScriptType;
+import org.bitcoinj.base.Sha256Hash;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.crypto.DeterministicKey;
+import org.bitcoinj.crypto.ECKey;
+import org.bitcoinj.signers.LocalTransactionSigner;
+import org.bitcoinj.signers.TransactionSigner;
+import org.bitcoinj.wallet.DeterministicKeyChain;
+import org.bitcoinj.wallet.KeyBag;
+import org.bitcoinj.wallet.KeyChainGroup;
+import org.bitcoinj.wallet.RedeemData;
+import org.jetbrains.annotations.Nullable;
+
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BitcoinConfigurationStackedService extends AbstractConfigurationStackedService implements IBitcoinConfiguration {
     public final BitcoinConfiguration config;
-    public final BitcoinMasterService bms;
+    private final KeyVaultProxy.BitcoinProtocolExecutor executor;
+    private final KeyChainGroup wkcg;
+
+    class WrapedKeyBag implements KeyBag {
+        @Nullable
+        @Override
+        public ECKey findKeyFromPubKeyHash(byte[] bytes, @Nullable ScriptType scriptType) {
+            ECKey keyFromPubKeyHash = wkcg.findKeyFromPubKeyHash(bytes, scriptType);
+
+            if (keyFromPubKeyHash == null) {
+                return null;
+            }
+
+            return executor.new WrapedEcKey(keyFromPubKeyHash.getPubKeyPoint(), keyFromPubKeyHash.isCompressed(), scriptType);
+        }
+
+        @Nullable
+        @Override
+        public ECKey findKeyFromPubKey(byte[] bytes) {
+            return wkcg.findKeyFromPubKey(bytes);
+        }
+
+        @Nullable
+        @Override
+        public RedeemData findRedeemDataFromScriptHash(byte[] bytes) {
+            return wkcg.findRedeemDataFromScriptHash(bytes);
+        }
+    }
 
     public BitcoinConfigurationStackedService(BitcoinProtocolStackedService parent, BitcoinConfiguration config) {
         super(parent, new String(WalletHelper.mangle(TestWallet.ConfigurationHelper.toJSON(config))));
         this.config = config;
-        bms = new BitcoinMasterService(parent.parent.proxy.new BitcoinProtocolExecutor(config), config);
+        this.executor = parent.parent.proxy.new BitcoinProtocolExecutor(config);
+
+        KeyChainGroup.Builder kcgb = KeyChainGroup.builder(config.network());
+        DeterministicKey watchKey = DeterministicKey.deserializeB58(executor.getWatchingKey(), config.network());
+
+        for (ScriptType outputScriptType : config.scriptTypes()) {
+            DeterministicKeyChain chain = DeterministicKeyChain.builder().watch(watchKey).outputScriptType(outputScriptType).build();
+            chain.setLookaheadSize(100);
+            chain.maybeLookAhead();
+            kcgb.addChain(chain);
+        }
+
+        this.wkcg = kcgb.build();
     }
 
     @Override
     public BitcoinProtocolMessages.GetWatchingKeyAccept getWatchingKey() {
-        return bms.getWatchingKey();
+        return new BitcoinProtocolMessages.GetWatchingKeyAccept(
+                executor.getWatchingKey(),
+                executor.config.scriptTypes(),
+                executor.config.network());
     }
 
     @Override
     public BitcoinProtocolMessages.BitcoinTransactionSignatureAccept signTransaction(BitcoinProtocolMessages.BitcoinTransactionSignatureRequest request) {
-        return bms.signTransaction(request);
-    }
+        Transaction transaction = Transaction.read(ByteBuffer.wrap(request.tx()));
 
-//    @Override
-//    protected String getIdString() {
-//        return TestWallet.ConfigurationHelper.toJSON(config);
-//    }
+        // Convert the binary map into Objects
+        Map<Sha256Hash, Transaction> transactionMap = request.map().entrySet().stream().collect(Collectors.toUnmodifiableMap(
+                entry -> Sha256Hash.wrap(entry.getKey()),
+                entry -> Transaction.read(ByteBuffer.wrap(entry.getValue()))));
+
+        // Connect the inputs in the request to the outputs
+        transaction.getInputs().forEach(ti -> {
+            ti.connect(transactionMap.get(ti.getOutpoint().hash()).getOutput(ti.getOutpoint().index()));
+        });
+
+        // Prompts the user
+        System.out.println("transaction: " + transaction);
+
+        //Yey, we are
+        TransactionSigner.ProposedTransaction pt = new TransactionSigner.ProposedTransaction(transaction);
+        LocalTransactionSigner signer = new LocalTransactionSigner();
+        signer.signInputs(pt, new WrapedKeyBag());
+        return new BitcoinProtocolMessages.BitcoinTransactionSignatureAccept(transaction.serialize());
+    }
 }
